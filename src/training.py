b682ac44f7a335a2305188814c5ec142d85ea6d2
jurek <jerzy.dabrowski@lekta.ai>
2022-04-23 15:50:43 +0200

    Added test loop

--- src/training.py
+++ src/training.py
@@ -29,3 +29,19 @@ for epoch in tqdm(range(num_epochs)):
         if (i+1) % 100 == 0:
             print(f"Epoch {epoch+1}, loss = {loss.item():.5f}")
 
+with torch.no_grad():
+    num_correct = 0
+    n_samples = 0
+    for images, labels in test_dataloader:
+        images.to(device)
+        labels = torch.tensor(labels).to(device)
+        out = conv_net(images)
+
+        # get value and index of max elem
+        _, predictions = torch.max(out, 1)
+        n_samples += labels.size(0)
+        num_correct += (predictions == labels).sum().item()
+
+    acc = num_correct/n_samples
+    print(f'Accuracy: {acc}')
+
